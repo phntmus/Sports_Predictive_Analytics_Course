f749314420bbc995486eb13c592bbcd1a0342fcc
Dmitriy <dmitriy.isarov@gmail.com>
2026-05-20 21:30:39 +0300

    Fixed bugs 3

--- ViewModels/ManualPredictionRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace SportsAnalyticsWeb.ViewModels;
-
-// Запрос для ручного расчета коэффициентов
-public sealed class ManualPredictionRequest
-{
-    public int HomeTeamId { get; set; }
-    public int AwayTeamId { get; set; }
-    public double MarginPercent { get; set; } = 7;
-}
